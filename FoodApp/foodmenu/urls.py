1dbea8a233b0f969066eab8ed638546e8c1228ec
devrunner09 <devrunner09@gmail.com>
2023-07-01 13:37:27 +0700

    add CRUD items

--- FoodApp/foodmenu/urls.py
+++ FoodApp/foodmenu/urls.py
@@ -1,7 +1,16 @@
 from . import views
 from django.urls import path
 
+app_name = 'foodmenu'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail')
+    path('<int:item_id>/', views.detail, name='detail'),
+
+    # add items form
+    path('add', views.create_item, name='create_item'),
+    # edit
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # delete
+    path('delete/<int:id>/', views.delete_item, name='delete_item')
 ]
